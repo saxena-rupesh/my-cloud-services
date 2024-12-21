6d81ed7ad2a2c5c4de6ce127f92a01dfe5336f1e
saxena-rupesh <mail2rsaxena@gmail.com>
2024-12-21 16:35:42 +0530

    initial version of sample rest resvice

---
+++ sample-restful-service/src/main/java/com/saxenarupesh/sample_restful_service/SampleRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saxenarupesh.sample_restful_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestfulServiceApplication.class, args);
+	}
+
+}
---
+++ sample-restful-service/src/test/java/com/saxenarupesh/sample_restful_service/SampleRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxenarupesh.sample_restful_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
